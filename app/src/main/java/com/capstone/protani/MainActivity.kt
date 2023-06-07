7b7f9c1e1550039f424bd6c684e4f05f9819bc31
Muhammad Yaumil Ramadhani <muhammadyaumil1313@gmail.com>
2023-06-07 12:19:45 +0700

    permissions camera & external storage

--- app/src/main/java/com/capstone/protani/MainActivity.kt
+++ app/src/main/java/com/capstone/protani/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.capstone.protani
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.capstone.protani.ui.navigation.SetupNavGraph
 import com.capstone.protani.ui.theme.ProtaniTheme
 
 class MainActivity : ComponentActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if(isGranted){
+            Log.i("permissions","permissions Granted!")
+        }else{
+            Log.i("permissions","permissions Rejected!")
+        }
+    }
+    private fun requestCameraPermissions(){
+        when{
+            ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED->{
+                        Log.i("camera permission","Camera Permission previously Granted!")
+                    }
+            ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.CAMERA)
+                    ->Log.i("camera permission","show camera permissions dialog")
+            else->requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
+    private fun requestPermissionStorage(){
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                        Log.i("storage permission","storage permission previously granted!")
+                    }
+            ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)->Log.i("storage permission","Storage permission granted!")
+            else->requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -18,5 +54,7 @@ class MainActivity : ComponentActivity() {
                 SetupNavGraph(navController = controller)
             }
         }
+        requestPermissionStorage()
+        requestCameraPermissions()
     }
 }
