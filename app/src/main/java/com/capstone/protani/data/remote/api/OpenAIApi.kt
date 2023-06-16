03c882d5110ba8f2b0659b023ed7765cf2a77af8
Hanif Hanan Al-Jufri <87616338+Hansaljuf@users.noreply.github.com>
2023-06-16 18:20:48 +0700

    Update OpenAIApi.kt

--- app/src/main/java/com/capstone/protani/data/remote/api/OpenAIApi.kt
+++ app/src/main/java/com/capstone/protani/data/remote/api/OpenAIApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface OpenAIApi {
-    @Headers("Content-Type: application/json", "Authorization: Bearer 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")
+    @Headers("Content-Type: application/json", "Authorization: Bearer ")
     @POST("v1/chat/completions")
     suspend fun generateResponse(@Body requestBody: OpenAIRequestBody): OpenAIResponse
 }
